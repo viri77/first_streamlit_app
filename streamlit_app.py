c407b21d40969442ff64947d54fe952725fa1337
Olha Karpenko <viri77557@gmail.com>
2023-08-27 00:16:53 +0300

    function

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,11 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+def get_fruitvice_data(this_fruit_choice):
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" )
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        return fruityvice_normalized
+
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
 
@@ -35,9 +40,8 @@ try:
         streamlit.error("Please select fruit to get information.")
 
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_function = get_fruitvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
 
