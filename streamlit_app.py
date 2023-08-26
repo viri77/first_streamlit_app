2767c2cea0f3f2ab6518bda9160824cb330060c5
Olha Karpenko <viri77557@gmail.com>
2023-08-26 20:45:41 +0300

    new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.header("Fruityvice Fruit Advice!")
 
 
 # Let's put a pick list here so they can pick the fruit they want to include 
