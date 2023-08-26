91d8fba5476a9729bc7d7cb2ccd0fbe0618127ca
Olha Karpenko <viri77557@gmail.com>
2023-08-26 20:48:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json)
+streamlit.text(fruityvice_response.json())
 
 streamlit.title('My Parents New healthy Dinner')
 streamlit.header('Breakfast favorites')
