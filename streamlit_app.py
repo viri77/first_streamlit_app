ca7eea06f6dab450d41edd5b3c17e3d29ef7083e
Olha Karpenko <viri77557@gmail.com>
2023-08-26 20:51:06 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalize data 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# make dataframe
+streamlit.dataframe(fruityvice_normalized)
